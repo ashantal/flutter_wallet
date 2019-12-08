a2f9bb4e0d92bfd3dd39ba91a6580499e39c3d5d
ashantal <ashantal@hotmail.com>
2019-12-08 02:32:22 -0800

    working with SHA256 payload

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:pointycastle/digests/ripemd160.dart';
+import 'package:pointycastle/digests/sha256.dart';
 import 'package:pointycastle/ecc/curves/secp256k1.dart';
 import 'package:qrcode_reader/qrcode_reader.dart';
 import 'package:http/http.dart';
+import 'package:randombytes/randombytes.dart';
 import 'dart:convert';
 import 'package:sacco/sacco.dart';
 import 'package:my_app/service/ES256S.dart';
@@ -9,6 +14,9 @@ import 'package:my_app/service/jwt_service.dart';
 import 'package:hex/hex.dart';
 import 'package:bip39/bip39.dart' as bip39;
 import 'package:bip32/bip32.dart' as bip32;
+import 'package:keccak/keccak.dart' as keccak;
+import 'package:web3dart/crypto.dart';
+import 'package:web3dart/web3dart.dart' as w3;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -33,8 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   static String _mnemonic = "analyst end eye apple burden trust snack question feature monkey dinner loan";
   static String _path="m/7696500'/0'/0'/0'";
   final networkInfo = NetworkInfo(id:"ec", bech32Hrp: "ec", lcdUrl: "");
-  String _did = "did:ethr:0x199254bf2a7b5d0c705cdb1648f4165e64364696";
-  String _pk = "b3a39ebac976a45c9c439741ffa26bfd5fd0cdcabf4ccf2d627cc8b26d0f398a";
+  //String _did = "did:ethr:0x199254bf2a7b5d0c705cdb1648f4165e64364696";
   static String _jwt = 
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzU2OTc4OTAsImV4cCI6MTU3NTY5ODQ5MCwicmVxdWVzdGVkIjpbIm5hbWUiXSwidmVyaWZpZWQiOlsiVXBvcnRsYW5kaWEgQ2l0eSBJRCJdLCJjYWxsYmFjayI6Imh0dHBzOi8vOWVhOGRmN2Yubmdyb2suaW8vY2FsbGJhY2siLCJ0eXBlIjoic2hhcmVSZXEiLCJpc3MiOiJkaWQ6ZXRocjoweGJjM2FlNTliYzc2Zjg5NDgyMjYyMmNkZWY3YTIwMThkYmUzNTM4NDAifQ.kscGK47t25RDHDWB0-aJ8rgfpp67JlESf5o4gT2wq_Yl6V22arJz0tP8fUuJTzESE7XKWAuBO4OBvJbx7i3xUQA";  //me.uport:request/eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzQ4ODI5NzAsImV4cCI6MTU3NDg4MzA5MCwicmVxdWVzdGVkIjpbIm5hbWUiXSwidmVyaWZpZWQiOlsiVXBvcnRsYW5kaWEgQ2l0eSBJRCJdLCJwZXJtaXNzaW9ucyI6WyJub3RpZmljYXRpb25zIl0sImNhbGxiYWNrIjoiaHR0cHM6Ly9hcGkudXBvcnQubWUvY2hhc3F1aS90b3BpYy9UWXpNVnZEUXoiLCJ2YyI6WyIvaXBmcy9RbWRXbnNnRDlOdVFjQmF1VThlQXJ4Uk1Da2JEUTQycThtaUNoYjZ3b0htUlRSIl0sImFjdCI6Im5vbmUiLCJ0eXBlIjoic2hhcmVSZXEiLCJpc3MiOiJkaWQ6ZXRocjoweGFiMjU4YTE3MjU2Y2NlZGI5MjJkNjgwYTVkZDIwNGJhNmI5ODFmMDkifQ.N72Nfug-YiEXemCtLJfApJlb0mGvX-YXGGv2JQ1zTz6ZOh7xLxEuQ4Itgc7hG7jD4mWrkL-P9kT_mweQRrivBAE";
   Future<String> _barcodeString;
@@ -46,35 +53,17 @@ class _MyHomePageState extends State<MyHomePage> {
     return JWT.parse(path[0]);
   }
 
-  Future<String> issueJWT() async{
+  Future<String> issueJWT(String did, Uint8List privateKey) async{
     var builder = new JWTBuilder()
-      ..issuer = _did
-      ..audience='did:ethr:0xbc3ae59bc76f894822622cdef7a2018dbe353840'
+      ..issuer = did //'did:ethr:0xb9c5714089478a327f09197987f16f9e5d936e8a'
+      ..audience= did //'did:ethr:0xbc3ae59bc76f894822622cdef7a2018dbe353840'
       ..expiresAt = new DateTime.now().add(new Duration(minutes: 3))
       ..setClaim('type', 'shareResp')
       ..setClaim('own', {'name': 'Ash'})
       ..setClaim('req', _jwt);
-    Wallet wallet = Wallet.derive(_mnemonic.split(" "), _path, networkInfo);
 
-    //prv b3a39ebac976a45c9c439741ffa26bfd5fd0cdcabf4ccf2d627cc8b26d0f398a
-    //pub 029f1f4a350c5527220501e8aae1ff9992cb6a19b3440917bcd2f67852702d0984
-    ES256S signer = new ES256S(wallet.privateKey);
-
+    ES256S signer = new ES256S(privateKey);
     var signedToken = await builder.getSignedToken(signer);
-
-/*
-    var w=Wallet.derive(m.split(" "), "m/7696500'/0'/0'/0'", networkInfo);
-    print(base64.encode(w.publicKey));
-    print(base64.encode(w.address));
-    var w1=Wallet.derive(m.split(" "), "m/7696500'/1'/0'/0'", networkInfo);
-    print(base64.encode(w1.publicKey));
-    print(base64.encode(w1.address));
-    var w2=Wallet.derive(m.split(" "), "m/7696500'/2'/0'/0'", networkInfo);
-    print(base64.encode(w2.publicKey));
-    print(base64.encode(w2.address));
- */
-
-    print(signedToken);
     return signedToken.toString();
   }
 
@@ -101,30 +90,35 @@ final seed = bip39.mnemonicToSeed(_mnemonic);
 print("seed 0x${HEX.encode(seed)}");  
 final root = bip32.BIP32.fromSeed(seed);
 
-for(var a=0;a<4;a++){
-    for(var n=0;n<=4;n++){
-        for(var i=0;i<=4;i++){
-          var path = "m/7696500'/$a'/$n'/$i";
-          
-          /*var hdnode = root.derivePath(path);
-          var secp256k1 = ECCurve_secp256k1();
-          var point = secp256k1.G;
-          // Compute the curve point associated to the private key
-          var bigInt = BigInt.parse(HEX.encode(hdnode.privateKey), radix: 16);
-          var curvePoint = point * bigInt;
-          // Get the public key
-          var key0 = curvePoint.getEncoded();
-          print("$path 0x${HEX.encode(key0)}"); 
-*/
-          var key1 = Wallet.derive(_mnemonic.split(" "), path, networkInfo).publicKey;
-          print("$path 0x${HEX.encode(key1)}");
+var path = "m/7696500'/0'/0'/0";          
+var hdnode = root.derivePath(path);
+var secp256k1 = ECCurve_secp256k1();
+var point = secp256k1.G;
+// Compute the curve point associated to the private key
+var bigInt = BigInt.parse(HEX.encode(hdnode.privateKey), radix: 16);
+var curvePoint = point * bigInt;
 
-          //var add = Wallet.derive(_mnemonic.split(" "), path, networkInfo).address;
-          //print("address 0x${HEX.encode(add)}");
+//var creds = w3.EthPrivateKey.fromHex(HEX.encode(hdnode.privateKey));
+//var extractAddress = await creds.extractAddress();
+//print("did:ethr:${extractAddress.hex}");
 
-        }        
-    }
-}  
+// Get the public key
+//var pubKey = curvePoint.getEncoded(); //32 bit
+var pubBytes = curvePoint.getEncoded(false).sublist(1);
+const int shaBytes = 256 ~/ 8;
+var hashed = sha3digest.process(pubBytes);
+var address = Uint8List.view(hashed.buffer, shaBytes - 20);
+
+final did = "did:ethr:0x${HEX.encode(address)}";
+print("----- pub bytes -----");
+print("${HEX.encode(pubBytes)}");
+print("---------------------");
+print("$did");
+print("---------------------");
+
+var jwtResponse = await issueJWT(did,hdnode.privateKey);
+print("$jwtResponse");
+
   
   /*var jwtResponse = await issueJWT();
   
@@ -156,16 +150,6 @@ for(var a=0;a<4;a++){
               ),
       floatingActionButton: new FloatingActionButton(
         onPressed: () {
-
-                /*    
-                String seed = bip39.mnemonicToSeedHex(_mnemonic);
-                print(seed);
-                bip32.BIP32 root = bip32.BIP32.fromSeed(HEX.decode(seed));
-                bip32.BIP32 child = root.derivePath(_path);
-                String privateKey = HEX.encode(child.privateKey);
-                print(privateKey);
-                */
-
           setState(() {
             _barcodeString = new QRCodeReader()
                 .setAutoFocusIntervalInMs(200)
--- lib/service/ES256S.dart
+++ lib/service/ES256S.dart
@@ -1,6 +1,12 @@
 import 'dart:math';
 import 'dart:typed_data';
+import 'package:hex/hex.dart';
 import 'package:my_app/service/jwt_service.dart';
+import 'package:pointycastle/digests/sha256.dart';
+import 'package:pointycastle/ecc/curves/secp256k1.dart';
+import 'package:pointycastle/macs/hmac.dart';
+import 'package:pointycastle/pointycastle.dart';
+import 'package:pointycastle/signers/ecdsa_signer.dart';
 import 'package:web3dart/crypto.dart';
 import 'package:web3dart/web3dart.dart' as web3;
 class ES256S implements JWTSigner {
@@ -12,8 +18,6 @@ class ES256S implements JWTSigner {
   @override
   Future<List<int>> sign(List<int> data) async{
     final k = ECK(_key);
-    final key = web3.EthPrivateKey(_key);
-    //final key = ECK(Random.secure());
     List<int> sigList = await k.sign(Uint8List.fromList(data));
     return sigList;
   }
@@ -29,8 +33,135 @@ class ECK extends web3.EthPrivateKey{
   
   @override
   Future<MsgSignature> signToSignature(Uint8List payload, {int chainId}) async {
-    final signature = await super.signToSignature(payload); 
+ // final signature = await super.signToSignature(payload); 
 
-    return MsgSignature(signature.r, signature.s, signature.v-27);
+  final message = SHA256Digest().process(payload);
+  //final message = keccak256(payload);
+
+  final ECDomainParameters _params = ECCurve_secp256k1();
+  final BigInt _halfCurveOrder = _params.n >> 1;
+
+  final digest = SHA256Digest();
+  final signer = ECDSASigner(null, HMac(digest, 64));
+  final key = ECPrivateKey(bytesToInt(privateKey), _params);
+
+  signer.init(true, PrivateKeyParameter(key));
+  var sig = signer.generateSignature(message) as ECSignature;
+
+  /*
+	This is necessary because if a message can be signed by (r, s), it can also
+	be signed by (r, -s (mod N)) which N being the order of the elliptic function
+	used. In order to ensure transactions can't be tampered with (even though it
+	would be harmless), Ethereum only accepts the signature with the lower value
+	of s to make the signature for the message unique.
+	More details at
+	https://github.com/web3j/web3j/blob/master/crypto/src/main/java/org/web3j/crypto/ECDSASignature.java#L27
+	 */
+  if (sig.s.compareTo(_halfCurveOrder) > 0) {
+    final canonicalisedS = _params.n - sig.s;
+    sig = ECSignature(sig.r, canonicalisedS);
   }
+  final pubBytes = privateKeyBytesToPublic(privateKey);
+  final publicKey = bytesToInt(pubBytes);
+
+
+print("----- searching for pub bytes -----");
+print("${HEX.encode(pubBytes)}");
+print("$publicKey");
+print("-----------------------------------");
+
+  //Implementation for calculating v naively taken from there, I don't understand
+  //any of this.
+  //https://github.com/web3j/web3j/blob/master/crypto/src/main/java/org/web3j/crypto/Sign.java
+  var recId = -1;
+  for (var i = 0; i < 4; i++) {
+    final k = recoverFromSignature(i, sig, message, _params);
+    if (k == publicKey) {
+      recId = i;
+      break;
+    }
+  }
+
+  if (recId == -1) {
+    print('Could not construct a recoverable key. This should never happen');
+    throw Exception(
+        'Could not construct a recoverable key. This should never happen');
+  }
+
+
+
+    //var rBytes = intToBytes(sig.r).toList().reversed;
+    //var sBytes = intToBytes(sig.s).toList().reversed;
+    //bytes.setRange(0, 32, rBytes);
+    //bytes.setRange(32,64, sBytes);
+    //print(bytes);
+  
+  return MsgSignature(sig.r, sig.s, recId);
+    //return MsgSignature(signature.r, signature.s, 1/*signature.v-27*/);
+  }
+
+  int fromBytes(var rBytes){
+    var n=0;
+    final int8List = new Int8List(rBytes.length);
+    var list = rBytes.toList();
+    list.forEach((i)=>
+       int8List[n]=i
+    );
+    var bdata = new ByteData.view(int8List.buffer);
+    return bdata.getInt32(0);
+  }
+
+
+  BigInt recoverFromSignature(
+    int recId, ECSignature sig, Uint8List msg, ECDomainParameters params) {
+  final n = params.n;
+  final i = BigInt.from(recId ~/ 2);
+  final x = sig.r + (i * n);
+
+  //Parameter q of curve
+  final prime = BigInt.parse(
+      'fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f',
+      radix: 16);
+  if (x.compareTo(prime) >= 0) return null;
+
+  final R = _decompressKey(x, (recId & 1) == 1, params.curve);
+  if (!(R * n).isInfinity) return null;
+
+  final e = bytesToInt(msg);
+
+  final eInv = (BigInt.zero - e) % n;
+  final rInv = sig.r.modInverse(n);
+  final srInv = (rInv * sig.s) % n;
+  final eInvrInv = (rInv * eInv) % n;
+
+  final q = (params.G * eInvrInv) + (R * srInv);
+
+  final bytes = q.getEncoded(false);
+  return bytesToInt(bytes.sublist(1));
+}
+ECPoint _decompressKey(BigInt xBN, bool yBit, ECCurve c) {
+  List<int> x9IntegerToBytes(BigInt s, int qLength) {
+    //https://github.com/bcgit/bc-java/blob/master/core/src/main/java/org/bouncycastle/asn1/x9/X9IntegerConverter.java#L45
+    final bytes = intToBytes(s);
+
+    if (qLength < bytes.length) {
+      return bytes.sublist(0, bytes.length - qLength);
+    } else if (qLength > bytes.length) {
+      final tmp = List<int>.filled(qLength, 0);
+
+      final offset = qLength - bytes.length;
+      for (var i = 0; i < bytes.length; i++) {
+        tmp[i + offset] = bytes[i];
+      }
+
+      return tmp;
+    }
+
+    return bytes;
+  }
+
+  final compEnc = x9IntegerToBytes(xBN, 1 + ((c.fieldSize + 7) ~/ 8));
+  compEnc[0] = yBit ? 0x03 : 0x02;
+  return c.decodePoint(compEnc);
+}
 }
